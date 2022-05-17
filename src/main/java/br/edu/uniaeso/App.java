d07e665b95215321e6f622a2fe363a4326852c4f
C1325053 <allysson.oliveira@capgemini.com>
2022-05-16 21:28:20 -0300

    solução em mvc

--- src/main/java/br/edu/uniaeso/App.java
+++ src/main/java/br/edu/uniaeso/App.java
@@ -1,11 +1,13 @@
 package br.edu.uniaeso;
 
+import br.edu.uniaeso.view.TelaVotacao;
 /**
  * Hello world!
  *
  */
 public class App {
     public static void main(String[] args) {
+
         TelaVotacao tv = new TelaVotacao();
     }
 }
